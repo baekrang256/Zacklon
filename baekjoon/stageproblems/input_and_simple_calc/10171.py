b0557e6557188e78c440d42df507f9bd445c6e27
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2019-08-22 16:59:56 +0900

    Create 10171.py

---
+++ baekjoon/stageproblems/input_and_simple_calc/10171.py
@@ -0,0 +1,9 @@
+"""
+DATE : 08 - 22
+29056kb, 56ms
+"""
+
+print('''\    /\ ''') # you need to space out at the final part, or else since \' is used to make "'" a string, error will appear.
+print(''' )  ( ')''')
+print('''(  /  )''')
+print(''' \(__)|''')
