2bd5e970635a5c067753b7177d4d4c23629a9dbc
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2019-08-23 21:58:39 +0900

    Create 10869.py

---
+++ baekjoon/stageproblems/input_and_simple_calc/10869.py
@@ -0,0 +1,14 @@
+"""
+DATE - 2019/08/23
+29056kb, 56ms
+"""
+
+import sys
+
+a = sys.stdin.readline()
+b = a.split()
+print(int(b[0])+int(b[1]))
+print(int(b[0])-int(b[1]))
+print(int(b[0])*int(b[1]))
+print(int(b[0])//int(b[1]))
+print(int(b[0])%int(b[1]))
