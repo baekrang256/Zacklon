a7c834c42c8d2aec6b0ca7673eaaa7a3ffeee822
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2019-08-24 12:32:10 +0900

    Create 1330.py

---
+++ baekjoon/stageproblems/if/1330.py
@@ -0,0 +1,16 @@
+"""
+DATE - 2019/08/24
+29056kb, 56ms
+"""
+
+import sys
+
+numbers = [int(k) for k in (sys.stdin.readline()).split()]
+
+
+if numbers[0] > numbers[1]:
+    print('>')
+elif numbers[0] < numbers[1]:
+    print('<')
+else:
+    print ('==')
