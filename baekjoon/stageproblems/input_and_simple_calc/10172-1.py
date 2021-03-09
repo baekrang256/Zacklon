fac98318e9d1bea89dcfeca6e53361c09f010290
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-09 13:50:26 +0900

    Update and rename 10172.py to 10172-1.py

---
+++ baekjoon/stageproblems/input_and_simple_calc/10172-1.py
@@ -0,0 +1,12 @@
+"""
+DATE - 2019/08/22
+29056kb, 60ms
+"""
+
+"""
+solution no.1
+to prevent character '\' making an error when using '\n', we either space it, like below, or use '\\', which is used to indicate that we want to denote the character \, not the other.
+so if we use \ at the third line, we don't really need to space all of the \n like below.
+"""
+
+print('''|\_/| \n|q p|   /} \n( 0 )"""\ \n|"^"`    | \n||_/=\\\__|''')
