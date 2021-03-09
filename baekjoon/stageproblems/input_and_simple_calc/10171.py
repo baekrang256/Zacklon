3afcf469952c6d871b358ac044d32ca196afac0d
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-09 13:33:30 +0900

    Update 10171.py

--- baekjoon/stageproblems/input_and_simple_calc/10171.py
+++ baekjoon/stageproblems/input_and_simple_calc/10171.py
@@ -3,7 +3,7 @@ DATE : 2019 - 08 - 22
 29056kb, 56ms
 """
 
-print('''\    /\ ''') # you need to space out at the final part, or else since \' is used to make "'" a string, error will appear.
+print('''\    /\ ''') # you need to give space after the final character, or else it'll make an error since \' is used to make "'" a string.
 print(''' )  ( ')''')
 print('''(  /  )''')
 print(''' \(__)|''')
