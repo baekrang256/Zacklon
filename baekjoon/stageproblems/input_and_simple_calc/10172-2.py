b14515c59c36199cdc9330f360bc3ac0f28f7279
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-09 13:59:34 +0900

    Create 10172-2.py

---
+++ baekjoon/stageproblems/input_and_simple_calc/10172-2.py
@@ -0,0 +1,18 @@
+
+"""
+DATE - 2021/03/09
+29056kb, 60ms
+"""
+
+"""
+right now trying...
+"""
+
+multiline = '''
+    |\_/|
+    |q p|   /}
+    ( 0 )"""
+    |"^"`    |
+    ||_/=\\__|
+    '''
+print(multiline)
