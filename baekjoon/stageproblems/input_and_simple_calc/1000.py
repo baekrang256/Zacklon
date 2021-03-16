ded12341700ea536511018cee481cb36be98926c
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-17 07:26:09 +0900

    Update 1000.py

--- baekjoon/stageproblems/input_and_simple_calc/1000.py
+++ baekjoon/stageproblems/input_and_simple_calc/1000.py
@@ -3,8 +3,8 @@ DATE - 2019 - 08 - 22
 28776kb, 76ms
 """
 
-import sys
+import sys #importing sys to use readline()
 
 a = sys.stdin.readline()
-b = a.split()
+b = a.split() #splitting the input into list in terms of blank.
 print(int(b[0])+int(b[1]))
