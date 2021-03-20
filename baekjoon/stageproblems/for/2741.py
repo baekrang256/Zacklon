4592998b32f74bad49129ac831c294f4cc6a3380
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-20 21:13:39 +0900

    Update 2741.py

--- baekjoon/stageproblems/for/2741.py
+++ baekjoon/stageproblems/for/2741.py
@@ -9,3 +9,13 @@ n = int(sys.stdin.readline())
 
 for i in range(1,n+1):
     print(i)
+    
+"""
+DATE : 2021.3.20
+29887kb, 112ms
+
+little bit simpler solution
+"""
+a = int(input())
+for i in range(a):
+    print(i+1)
