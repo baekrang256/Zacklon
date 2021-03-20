9e91876de5a37cb5fa065e7af40c475e4f417b63
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-21 08:01:31 +0900

    Update 11022.py

--- baekjoon/stageproblems/for/11022.py
+++ baekjoon/stageproblems/for/11022.py
@@ -10,3 +10,31 @@ T = int(sys.stdin.readline())
 for i in range(1,T+1):
     num = [int(k) for k in (sys.stdin.readline()).split()]
     print("Case #%d: %d + %d = %d" % (i, num[0], num[1], num[0]+num[1]))
+
+"""
+2021.3.21
+28776kb,68ms
+
+used map
+"""
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(1,T+1):
+    num = [int(k) for k in (sys.stdin.readline()).split()]
+    print("Case #%d: %d" % (i, num[0]+num[1]))
+    
+"""
+2021.3.21
+28776kb, 80ms
+
+used input() and map()
+as expected, it takes a bit more time
+"""
+
+n = int(input())
+for i in range(1,n+1):
+    a, b = map(int, input().split())
+    print("Case #%d: %d + %d = %d" % (i,a,b,a+b))
