0378d6aa870875492e54ad6dc7dd2d88f3fd9011
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-20 21:11:24 +0900

    Update 15552.py

--- baekjoon/stageproblems/for/15552.py
+++ baekjoon/stageproblems/for/15552.py
@@ -14,3 +14,19 @@ T = int(sys.stdin.readline())
 for i in range(1, T+1):
     num = [int(k) for k in (sys.stdin.readline()).split()]
     print(num[0]+num[1])
+
+"""
+2021.3.20
+28776kb, 1464ms
+A little bit simpler solution.
+We were able to use input() for this function, surprisingly.
+however, it should be only used for the first line, not in the 'for' paragraph.
+The reason is, that would really make the algorithm go long.
+"""
+
+import sys
+
+a = int(input())
+for i in range(a):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a+b)
