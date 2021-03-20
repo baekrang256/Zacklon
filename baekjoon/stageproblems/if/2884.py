0deee56cbdff7da6756bbc40b276ddee15d27212
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-20 20:18:48 +0900

    Update 2884.py

--- baekjoon/stageproblems/if/2884.py
+++ baekjoon/stageproblems/if/2884.py
@@ -15,3 +15,28 @@ else:
     Time[1] += -45
 
 print("%d %d" % (Time[0], Time[1]))
+
+"""
+another solution
+2021.3.20
+28776kb, 72ms
+"""
+
+h, m = map(int, input().split())
+if m<45:
+    h-=1
+    if h == -1:
+        h = 23
+    m+=15
+else:
+    m-=45
+hh, mm = str(h), str(m)
+print(hh+" "+mm)
+
+"""
+The first code is abstract because of the use of formatting.
+For more about formatting, see the link below:
+https://wikidocs.net/13#_15
+the below code is abstract for not importing sys and not using readline()
+it can be handled easily by mapping and input() splitting
+"""
