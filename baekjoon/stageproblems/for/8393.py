2e6284473d05d70611aded748cd2608863a5da1a
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-20 20:36:20 +0900

    Update 8393.py

--- baekjoon/stageproblems/for/8393.py
+++ baekjoon/stageproblems/for/8393.py
@@ -12,3 +12,11 @@ for i in range(1,n+1):
     sum += i
 
 print(sum)
+
+# a little bit simpler calculation,
+# 2021.3.20, 28776kb, 64ms
+
+x = 0
+for i in range(1,int(input())+1):
+    x+=i
+print(x)
