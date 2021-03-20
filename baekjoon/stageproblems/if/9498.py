b60a5eb37903cc351daca5a7ccf3d986b200a208
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-20 19:37:29 +0900

    Update 9498.py

--- baekjoon/stageproblems/if/9498.py
+++ baekjoon/stageproblems/if/9498.py
@@ -12,3 +12,16 @@ elif 80<=scr<90: print('B')
 elif 70<=scr<80: print('C')
 elif 60<=scr<70: print('D')
 else: print('F')
+
+"""
+more simplified code below
+2021.3.20
+28776kb, 84ms
+"""
+
+a = int(input())
+if a>= 90: print("A")
+elif a>=80: print("B")
+elif a>=70: print("C")
+elif a>=60: print("D")
+else: print("F")
