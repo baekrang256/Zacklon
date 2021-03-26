8a79e39e1ec1c4284ae1532c33de940a5b7fa76d
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-27 07:58:14 +0900

    Update 10951.py

--- baekjoon/stageproblems/while/10951.py
+++ baekjoon/stageproblems/while/10951.py
@@ -16,14 +16,37 @@ while True:
 This problem is similar to #10952, except that it uses EOF for termination.
 It is important to know EOF for the language your learning since it is a important terminology about processing files.
 
-If you use read(), and if there exists EOF, then nothing happens.
-If EOF doesn't exist, read() will give exception.
+In python, if you use readline() when there is no line(readline() returns the line that hasn't been readed), it returns ''
+It is notified in this link :  https://wikidocs.net/26
+Also in python, if you use read() after reading the whole file(read() method reads the whole line in file), it returns ''
+This is notified in this link : https://docs.python.org/3/tutorial/inputoutput.html
 
-reading in chunks make read() return empty string instead of None
-I used readline to read the file in chunks(in exact, lines)
-
-one more thing to mention is that readline, readlines, and read is all different
-read reads the whole file into one string data
-readline read each next line(compared to the iterator) with \n
-readlines() makes the whole file into list with \n
+there is also a function called readlines()
+it returns the whole file into a single list, each element of the list distinguished by '\n'.
 """
+
+"""
+2021/3/27
+28776kb,68ms
+
+this used 'not'.
+for more about not, look at this link
+https://dojang.io/mod/page/view.php?id=2192
+if not line is True, then the algorithm breaks the while loop.
+So, the question is, when does 'line' becomes False so that 'not line' becomes True?
+The answer is, when it is an empty string.
+Note that string is NOT empty when there is a blank. An empty string is a string with no character, even the blank doesn't exist.
+An empty string is falsy in python, which means that python consider empty string to be False in boolean.
+So we can use it to check if we're at the EOF.
+"""
+
+import sys
+
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break
+    a, b = map(int, line.split())
+    print(a+b)
+    
+
