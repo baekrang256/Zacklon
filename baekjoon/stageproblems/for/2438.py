7772553f5a00a2223c2f5edd15236e6cbf035af1
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-27 06:46:59 +0900

    Update 2438.py

--- baekjoon/stageproblems/for/2438.py
+++ baekjoon/stageproblems/for/2438.py
@@ -9,3 +9,16 @@ N = int(sys.stdin.readline())
 
 for i in range(1,N+1):
     print("*"*i)
+    
+"""
+2021.3.27
+28776kb, 72ms
+
+another way
+"""
+
+n = int(input())
+s = '*'
+for i in range(0,n):
+    print(s)
+    s += '*'
