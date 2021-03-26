2f9655a51c4fb548a7d49baedafbdb83743d0124
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-27 07:11:21 +0900

    Update 10871.py

--- baekjoon/stageproblems/for/10871.py
+++ baekjoon/stageproblems/for/10871.py
@@ -22,3 +22,22 @@ print(string.rstrip())
 for more information about rstrip, visit the following link
 https://docs.python.org/3/library/stdtypes.html?highlight=rstrip#text-sequence-type-str
 """
+
+"""
+2021.3.27
+29284kb, 72ms
+
+the thing is, because of baekjoon's scoring method, you don't really need to use rstrip().
+It'll give you answer even wtihout rstrip().
+Also, using map will make the list simpler.
+"""
+
+import sys
+
+n, k = map(int, input().split())
+l = sys.stdin.readline().split()
+ans = ''
+for i in range(0,n):
+    if int(l[i]) < k:
+        ans += '%s ' %l[i]
+print(ans)
