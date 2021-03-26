482b2ce541edff0bc6fce138501885497fd70596
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-27 06:55:57 +0900

    Update 2439.py

--- baekjoon/stageproblems/for/2439.py
+++ baekjoon/stageproblems/for/2439.py
@@ -13,3 +13,14 @@ for i in range(1, width+1):
 
 
 #for information about rjust(), visit https://docs.python.org/3/library/stdtypes.html?highlight=rjust#str.rjust
+
+"""
+2021.3.27
+28776kb, 68ms
+
+another way, not using rjust.
+"""
+
+n = int(input())
+for i in range(1,n+1):
+    print(' '*(n-i)+'*'*i)
