3b5b399623a3b725699688d6c7c2768710c1165e
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-03-28 18:30:45 +0900

    Create 8958.py

---
+++ baekjoon/stageproblems/list/8958.py
@@ -0,0 +1,27 @@
+"""
+2021.3.28
+28776kb, 76ms
+
+We didn't used list(???)
+Well it did used iteration.
+the row variable shows how much answer we got right in a row.
+we first count the number of case, then we start the 'for' loop N(number of case) times.
+score starts with 0, row starts with 1, and the result of each cases will be handled by the string variable 'result'.
+every time if we get the answer, we first add the score with the amount of rows of answers we got right.
+And then we add the number of row.
+If we got wrong, the row returns into 1.
+"""
+
+import sys
+N = int(sys.stdin.readline())
+for i in range(N):
+    score = 0
+    row = 1
+    result = sys.stdin.readline()
+    for i in range(len(result)):
+        if result[i] == 'O':
+            score += row
+            row += 1
+        else:
+            row = 1
+    print(score)
