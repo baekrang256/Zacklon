5b2fc7a8e73c3aeaf83751cc4aace0aa36465fff
baekrang256 <54342300+baekrang256@users.noreply.github.com>
2021-05-18 13:35:37 +0900

    Create 11654.py

---
+++ baekjoon/stageproblems/string/11654.py
@@ -0,0 +1,8 @@
+"""
+2021.5.18
+28776kb, 80ms
+used the ord function to turn ascii character into integer
+https://hashcode.co.kr/questions/201/python%EC%97%90%EC%84%9C-%EB%AC%B8%EC%9E%90%EC%9D%98-ascii%EA%B0%92
+"""
+
+print(ord(input()))
